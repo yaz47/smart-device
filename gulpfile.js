3c13d67461abae09b12d8b3d18e082535e4bb358
yaz47 <zayarnyy74@gmail.com>
2020-03-19 00:15:21 +0500

    Исправил баги, не относящиеся к пиксель перфекту

--- gulpfile.js
+++ gulpfile.js
@@ -90,7 +90,8 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source//*.ico"
+    "source//*.ico",
+    "source//*.png"
     ], {
       base: "source"
     })
